2da47df3b9f4e49524ab10b54ced22a28c5702aa
fredowinz23 <fredowinz23@gmail.com>
2018-07-07 18:45:11 +0800

    Fixed

--- pages/home_main.php
+++ pages/home_main.php
@@ -1,6 +1,6 @@
 <center>
   <br><br>
-  <h1>The APP that let's you confess anonymously</h1>
+  <h1>The APP that lets you confess anonymously</h1>
   <button class="btn orange" onclick="location.href='?view=register'">Sign up</button>
   <br>
   <a href="https://play.google.com/store/apps/details?id=com.ph.confession">
