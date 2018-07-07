f63512bf444a20b0d55b0a27757e0a56fdaaee6a
fredowinz23 <fredowinz23@gmail.com>
2018-07-07 18:59:56 +0800

    Layout

--- pages/home_main.php
+++ pages/home_main.php
@@ -3,7 +3,7 @@
   <h1>The APP that lets you confess anonymously</h1>
   <button class="btn orange" onclick="location.href='?view=register'">Sign up</button>
   <br><br>
-  Beta version available in google play<br>
+  Beta version available on Google play<br>
   <a href="https://play.google.com/store/apps/details?id=com.ph.confession">
     <img src="include/img/google_play.png" height="80px">
   </a>
