c63f351e1e2f4bf1c18749575381cc74d2b979d5
fredowinz23 <fredowinz23@gmail.com>
2018-08-07 11:41:45 +0800

    Added last change

--- mobile/getAll.php
+++ mobile/getAll.php
@@ -1,13 +1,14 @@
 <?php
 
+$auth = (isset($_POST['auth']) && $_POST['auth'] != '') ? $_POST['auth'] : '';
+$lastChange = (isset($_POST['lastChange']) && $_POST['lastChange'] != '') ? $_POST['lastChange'] : 0;
+
 $list = array();
-$list["confession"] = confession()->list();
+$list["confession"] = confession()->list("Id>$lastChange");
 $list["comment"] = comment()->list();
 $list["notification"] = notification()->list();
 $list["relate"] = relate()->list();
 $list["user"] = user()->list();
-
-$auth = (isset($_POST['auth']) && $_POST['auth'] != '') ? $_POST['auth'] : '';
 
 if ($auth=="phconfession123456789sunsetcity"){
   echo json_encode($list);
